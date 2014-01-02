55f1388d45ca4dbf0e94584dfb39d48bd0cb515b
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-01-02 01:21:02 +0100

    login form

--- app/i18n/en.js
+++ app/i18n/en.js
@@ -1,6 +1,10 @@
 var en = {};
-en.nginios = {};
-
-en.nginios.hello = 'Hello!';
+en.username_email = 'Username';
+en.password = 'Password';
+en.login = 'Login';
+en.forgot_password = 'Forgot your password?';
+en.enter_email = 'Email address';
+en.remember_your_email = 'If you remember your Email address, you can request a link to reset your password.';
+en.request_link = 'Request link'
 
 module.exports = en;
---
+++ public/js/nginios/main.js
@@ -0,0 +1,8 @@
+jQuery( function($){
+
+  $('.tooltip').qtip({
+    style: { classes: 'qtip-tipsy' },
+    position: { my: 'top center', at: 'bottom center', adjust: { y: 5 } }
+  });
+
+});
