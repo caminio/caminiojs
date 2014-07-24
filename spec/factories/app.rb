464f62e2ee553048628326f4a7cea1c65dc23a80
David Reinisch <david.reinisch@tastenwerk.com>
2014-07-24 17:10:13 +0200

    started with app_plan spec model

---
+++ spec/factories/app.rb
@@ -0,0 +1,23 @@
+#
+# @Author: David Reinisch
+# @Company: TASTENWERK e.U.
+# @Copyright: 2014 by TASTENWERK
+#
+# @Date:   2014-07-24 17:01:24
+#
+# @Last Modified by:   David Reinisch
+# @Last Modified time: 2014-07-24 17:02:16
+#
+# This source code is not part of the public domain
+# If server side nodejs, it is intendet to be read by
+# authorized staff, collaborator or legal partner of
+# TASTENWERK only
+FactoryGirl.define do
+
+  factory :app do
+    name "a app"
+    path "/a/content/path"
+  end
+
+end
+
---
+++ spec/models/app_plan_spec.rb
@@ -0,0 +1,56 @@
+#
+# @Author: David Reinisch
+# @Company: TASTENWERK e.U.
+# @Copyright: 2014 by TASTENWERK
+#
+# @Date:   2014-07-24 16:58:50
+#
+# @Last Modified by:   David Reinisch
+# @Last Modified time: 2014-07-24 17:09:36
+#
+# This source code is not part of the public domain
+# If server side nodejs, it is intendet to be read by
+# authorized staff, collaborator or legal partner of
+# TASTENWERK only
+
+require 'spec_helper'
+
+describe 'app_plan' do
+
+  context "attributes" do
+
+    let(:app) { create(:app) }
+
+  end
+
+  context "creation" do
+
+    it "must have an existing app"
+
+    it "can only be created by admin"
+  
+  end
+
+  context "usage" do
+
+    it "can be choosen by user"
+
+    it "must be confirmed if its not a free plan"
+
+  end
+
+  context "visibility" do
+
+    it "is visible if public is set"
+
+    it "can only be changed by admin"
+
+  end
+
+  context "destroy" do
+
+    it "cannot be destroyed if an user is using this plan"
+
+  end
+
+end
