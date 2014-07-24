5c94e0d2a543e49326a0a0769697371898b8580e
David Reinisch <david.reinisch@tastenwerk.com>
2014-07-24 17:34:59 +0200

    started with spec models

--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -6,7 +6,7 @@
 # @Date:   2014-07-24 17:11:47
 #
 # @Last Modified by:   David Reinisch
-# @Last Modified time: 2014-07-24 17:23:10
+# @Last Modified time: 2014-07-24 17:34:26
 #
 # This source code is not part of the public domain
 # If server side nodejs, it is intendet to be read by
@@ -19,11 +19,29 @@ describe 'app' do
 
   context "attributes" do
 
+    context "public" do
+
+      it "is set to false by default"
+
+    end
+
+    context "path" do
+
+      it "is set to a default path by default"
+
+    end
 
   end
 
   context "creation" do
         
+    it "is no set public"
+
+  end
+
+  context "destroy" do
+
+    ît "can only be destroyed if no app_plan is connected to this app"
 
   end
 
