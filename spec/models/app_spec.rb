da70dc774ea33ff07ec1ffc4fd4b0c1898e195e4
David Reinisch <david.reinisch@tastenwerk.com>
2014-07-24 17:23:37 +0200

    started with spec models

---
+++ spec/models/app_spec.rb
@@ -0,0 +1,30 @@
+#
+# @Author: David Reinisch
+# @Company: TASTENWERK e.U.
+# @Copyright: 2014 by TASTENWERK
+#
+# @Date:   2014-07-24 17:11:47
+#
+# @Last Modified by:   David Reinisch
+# @Last Modified time: 2014-07-24 17:23:10
+#
+# This source code is not part of the public domain
+# If server side nodejs, it is intendet to be read by
+# authorized staff, collaborator or legal partner of
+# TASTENWERK only
+
+require 'spec_helper'
+
+describe 'app' do
+
+  context "attributes" do
+
+
+  end
+
+  context "creation" do
+        
+
+  end
+
+end
