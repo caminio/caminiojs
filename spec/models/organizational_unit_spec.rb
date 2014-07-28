29fe8c5e32ffa43c519014b5fdb238760e60a6fe
David Reinisch <david.reinisch@tastenwerk.com>
2014-07-28 16:13:23 +0200

    proves message app

---
+++ spec/models/organizational_unit_spec.rb
@@ -0,0 +1,34 @@
+#
+# @Author: David Reinisch
+# @Company: TASTENWERK e.U.
+# @Copyright: 2014 by TASTENWERK
+#
+# @Date:   2014-07-28 15:15:01
+#
+# @Last Modified by:   David Reinisch
+# @Last Modified time: 2014-07-28 15:18:07
+#
+# This source code is not part of the public domain
+# If server side nodejs, it is intendet to be read by
+# authorized staff, collaborator or legal partner of
+# TASTENWERK only
+
+require 'spec_helper'
+
+describe 'organizational_unit' do
+
+  context "attributes" do
+
+    context "users" do
+
+    end
+
+
+    context "app_plans" do
+
+    end
+
+  end
+
+
+end
