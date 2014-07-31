d6b0fab980956c89b4e5e6789737e078be25037d
David Reinisch <david.reinisch@tastenwerk.com>
2014-07-31 13:45:54 +0200

    authorization tests working

--- spec/api/user_spec.rb
+++ spec/api/user_spec.rb
@@ -2,7 +2,7 @@
 # @Author: David Reinisch
 # @Date:   2014-07-29 18:29:18
 # @Last Modified by:   David Reinisch
-# @Last Modified time: 2014-07-31 10:36:56
+# @Last Modified time: 2014-07-31 13:44:52
 
 require "spec_helper"
 
@@ -14,42 +14,70 @@ describe "user api integration" do
     Users::API
   end
 
-  # before(:all) do
-  #   User.where({}).load.delete_all
-  #   @user = User.create(attributes_for(:user))
-  #   api_key = @user.api_keys.create
-  #   @auth = {'HTTP_AUTHORIZATION' => "Bearer #{api_key.access_token}", 'HTTP_UNIT' => "TEST"}
-  # end
+  before(:all) do
+    User.where({}).load.delete_all
+    @user = User.create(attributes_for(:user))
+    api_key = @user.api_keys.create
+    @auth = {'HTTP_AUTHORIZATION' => "Bearer #{api_key.access_token}", 'HTTP_UNIT' => "TEST"}
+    @unit = @user.organizational_units.first
+  end
 
-  # it "GET /" do
-  #   get "/", nil, @auth
-  #   # expect( last_response.body ).to eq( { :ping => "pong" }.to_json )
-  # end
+  context "GET /" do
 
-  # context "GET /:id" do
+    it "returns a json of all users of the current organizational unit" do
+      get "/", nil, @auth
+      # expect( last_response.body ).to eq( { :ping => "pong" }.to_json )
+    end
 
-  #   it "returns a json of the user gathered by id" do
-  #     get "/"+@user.id.to_s, nil, @auth
-  #     puts JSON.parse( last_response.body )['user']['id'].inspect
-  #     puts @user.id
-  #     # expect( JSON.parse( last_response.body )[:user] ).to eq( @user )
-  #   end
-
-  #   it "returns an error if no valid token is passed"
-
-  # end
-
-  # context "GET /:id/apps" do
+    it "returns an error if no valid token is passed" do
+      get "/"
+      expect( last_response.body ).to eq( unauthorized_error )
+    end
 
 
-  #   it "resturns a json of the users allowed apps in the current organizational unit" do
-  #     puts "/"+@user.id.to_s+"/apps"
-  #     get "/"+@user.id.to_s+"/apps", nil, @auth
-  #   # expect( last_response.body ).to eq( { :ping => "pong" }.to_json )
-  #   end
+  end
 
-  #   it "returns an error if no valid token is passed"
+  context "GET /:id" do
 
-  # end
+    it "returns a json of the user gathered by id" do
+      get "/"+@user.id.to_s, nil, @auth
+      expect( JSON.parse( last_response.body )['user']['id'] ).to eq( @user.id )
+    end
+
+    it "returns an error if no valid token is passed" do
+      get "/"+@user.id.to_s
+      expect( last_response.body ).to eq( unauthorized_error )
+    end
+
+  end
+
+  context "GET /:id/apps" do
+
+
+    it "resturns a json of the users allowed apps in the current organizational unit" do
+      get "/"+@user.id.to_s+"/apps", nil, @auth
+    # expect( last_response.body ).to eq( { :ping => "pong" }.to_json )
+    end
+
+    it "returns an error if no valid token is passed"do
+      get "/"+@user.id.to_s+"/apps"
+      expect( last_response.body ).to eq( unauthorized_error )
+    end
+
+  end
+
+
+  context "/:id/profile_picture" do
+
+    it "returns the profile picture of the user" do
+
+    end
+
+    it "returns an errror if no valid token is passed"
+  end
+
+  def unauthorized_error
+     "{\"error\":\"Unauthorized. Invalid or expired token.\"}"
+  end
 
 end
