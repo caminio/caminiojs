b0a16d95d0967df71ea20db602c9ffcaba4e9519
David Reinisch <david.reinisch@tastenwerk.com>
2014-08-05 11:19:03 +0200

    merge

---
+++ app/api/labels/api.rb
@@ -0,0 +1,13 @@
+class Users::API < Grape::API
+
+  version 'v1', using: :header, vendor: 'caminio', cascade: false
+  format :json
+  default_format :json
+
+  helpers Caminio::API::Helpers
+
+  get '/' do
+    authenticate!
+  end
+
+end
