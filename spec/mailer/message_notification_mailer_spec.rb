47db77df11b2270d92a63db4587a6d34256e7808
David Reinisch <david.reinisch@tastenwerk.com>
2014-08-08 22:18:11 +0200

    basic mailing works

--- spec/mailer/message_notification_mailer_spec.rb
+++ spec/mailer/message_notification_mailer_spec.rb
@@ -7,8 +7,9 @@ describe "message_notification_mailer"  do
     ActionMailer::Base.perform_deliveries = true
     ActionMailer::Base.deliveries = []
     @user = create(:user)
+    @user2 = create(:user)
     @message = Message.create_with_user(@user, { :content => 'Testcontent', :title => 'test' } )
-    puts MessageNotificationMailer.create_notification( @user, @message ).deliver
+    MessageNotificationMailer.create_notification( @user2, @message ).deliver
   end
 
   after(:each) do
@@ -17,6 +18,11 @@ describe "message_notification_mailer"  do
 
   it 'should send an email' do
     expect( ActionMailer::Base.deliveries.count ).to eq(1)
+  end
+
+  it 'renders the receiver email' do
+    puts ActionMailer::Base.deliveries.first.inspect
+    expect( ActionMailer::Base.deliveries.first.to.first ).to eq( @user2.email )
   end
   
   context "gets:" do
