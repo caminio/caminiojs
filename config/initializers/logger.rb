72ecb1827fc11825dac195a1dcc33b53738ba547
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-08-08 09:43:45 +0200

    removed logging for test mode

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,6 +1,8 @@
 if Rails.env == 'production'
   Rails.logger = Logger.new(Rails.root.join("log/caminio.log"))
 else
+  if Rails.env == 'development'
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
   Rails.logger = Logger.new(STDOUT)
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
