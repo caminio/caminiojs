f9a86831857b2b8ae07325c5b943cfb5ea1f3cb0
David Reinisch <david.reinisch@tastenwerk.com>
2014-08-11 10:41:12 +0200

    german - english mailing works

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,23 @@ class UserMailer < ActionMailer::Base
   def reset_password(user, link)
     @user = user
     @link = link
+    puts "USER LANG"
+    puts @user.locale
+    puts "OTHER"
+    puts I18n.locale
+    reset = I18n.locale
+    I18n.locale = @user.locale
     mail to: user.email, subject: I18n.t('caminio_password_reset')
+    I18n.locale = reset
   end
 
   def welcome(user, link)
     @user = user
     @link = link
+    reset = I18n.locale
+    I18n.locale = @user.locale
     mail to: user.email, subject: I18n.t('caminio_welcome')
+    I18n.locale = reset
   end
 
 end
--- spec/mailer/user_mailer_spec.rb
+++ spec/mailer/user_mailer_spec.rb
@@ -4,12 +4,17 @@ describe 'user_mailer' do
 
   context 'reset password' do
 
-    before(:each) do
+     before(:each) do
       ActionMailer::Base.delivery_method = :test
       ActionMailer::Base.perform_deliveries = true
       ActionMailer::Base.deliveries = []
       @user = create(:user)
-      UserMailer.reset_password( @user, 'link' ).deliver
+      @user.locale = 'de'
+      @user.save
+      @user2 = create(:user)
+      @user2.locale = 'en'
+      @user2.save
+      UserMailer.reset_password( @user, 'the link' ).deliver
     end
 
     after(:each) do
@@ -21,49 +26,14 @@ describe 'user_mailer' do
     end
 
     it 'renders the receiver email' do
-      expect( ActionMailer::Base.deliveries.first.to.first ).to eq( @user2.email )
-    end
-  
-    context "gets:" do
-
-      it "user"
-
-      it "link"
-
+      expect( ActionMailer::Base.deliveries.first.to.first ).to eq( @user.email )
     end
   
   end
 
   context 'welcome' do
 
-    before(:each) do
-      ActionMailer::Base.delivery_method = :test
-      ActionMailer::Base.perform_deliveries = true
-      ActionMailer::Base.deliveries = []
-      @user = create(:user)
-      UserMailer.welcome( @user, 'link' ).deliver
-    end
-
-    after(:each) do
-      ActionMailer::Base.deliveries.clear
-    end
-
-    it 'should send an email' do
-      expect( ActionMailer::Base.deliveries.count ).to eq(1)
-    end
-
-    it 'renders the receiver email' do
-      puts ActionMailer::Base.deliveries.first.inspect
-      expect( ActionMailer::Base.deliveries.first.to.first ).to eq( @user2.email )
-    end
   
-    context "gets:" do
-
-      it "user"
-
-      it "link"
-
-    end
 
   end
 
