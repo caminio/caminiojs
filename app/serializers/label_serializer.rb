22ab5434608df6b42435ca63c5edced282e18415
David Reinisch <david.reinisch@gmx.at>
2014-10-28 17:11:53 +0100

    merge

--- app/serializers/label_serializer.rb
+++ app/serializers/label_serializer.rb
@@ -1,3 +1,8 @@
 class LabelSerializer < ActiveModel::Serializer
   attributes :id, :name, :fgcolor, :bgcolor, :bdcolor, :category
+  has_one :organizational_unit
+
+  def organizational_unit
+    object.access_rules.first.organizational_unit
+  end
 end
