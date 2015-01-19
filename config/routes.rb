3bdd404c48841906e321c4bd249f9b46ed4bc50a
David Reinisch <david.reinisch@gmx.at>
2015-01-19 14:06:54 +0100

    mediafiles api

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   mount Caminio::V1::Users => "/api/v1/users"
   mount Caminio::V1::Organizations => "/api/v1/organizations"
   mount Caminio::V1::Locations => "/api/v1/locations"
+  mount Caminio::V1::Mediafiles => "/api/v1/mediafiles"
 
 end
