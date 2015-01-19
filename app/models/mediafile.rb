5f1f98cec45e188f84cc3c90a91231ad8a2a976b
David Reinisch <david.reinisch@gmx.at>
2015-01-19 13:28:14 +0100

    paperclip

---
+++ app/models/mediafile.rb
@@ -0,0 +1,42 @@
+class Mediafile
+  include Mongoid::Document
+  include Caminio::Timestamps
+  include Caminio::Userstamps
+
+  include Mongoid::Paperclip
+
+  has_mongoid_attached_file :file,
+    # path:         ':file/:id/:style.:extension',
+    styles: {
+      original: ['1920x1680>'],
+      thumb: ['100x100#']
+    }
+
+  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+
+  field :category, type: String
+  field :description, type: String
+  field :copyright, type: String
+
+  field :parent_id, type: BSON::ObjectId
+  field :parent_type, type: String
+  field :is_public, type: Boolean, default: true
+
+  def file_thumb
+    file.url(:thumb)
+  end
+  def file_original
+    file.url(:original)
+  end
+
+  def parent
+    return unless parent_type && parent_id
+    parent_type.constantize.find(parent_id)
+  end
+
+  def parent=(parent)
+    self.parent_type = parent.class.name
+    self.parent_id = parent.id
+  end
+
+end
