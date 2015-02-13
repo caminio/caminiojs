64b9ad201e37bb887d3e90c7a5f62ea6c9ea435a
thorsten zerha <thorsten.zerha@tastenwerk.com>
2015-02-13 15:37:41 +0100

    userstamp sets user or api_key as creator

--- lib/caminio/userstamps.rb
+++ lib/caminio/userstamps.rb
@@ -8,16 +8,21 @@ module Caminio
     included do
 
       belongs_to :created_by, class_name: 'User', inverse_of: nil
+      belongs_to :created_by_api_key, class_name: 'ApiKey', inverse_of: nil
       belongs_to :updated_by, class_name: 'User', inverse_of: nil
 
-      before_create :set_created_by
+      before_create :set_creator
       before_save :set_updated_by
 
       protected
 
-      def set_created_by
-        return unless current_user = RequestStore.store[:current_user]
-        self.created_by = current_user
+      def set_creator
+        if current_api_key = RequestStore.store[:current_api_key]
+          return self.created_by_api_key = current_api_key
+        if current_user = RequestStore.store[:current_user]
+          return self.created_by = current_user
+        end
+        raise 'Caminio API-Key constraint missing: neither current_api_key nor current_user is present in request_store'
       end
 
       def set_updated_by
