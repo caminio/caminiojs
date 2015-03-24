9fb070712e5d26d76461be55f368bd1fa21e820e
David Reinisch <david.reinisch@gmx.at>
2015-03-25 00:04:03 +0100

    up

--- app/helpers/caminio/auth_helper.rb
+++ app/helpers/caminio/auth_helper.rb
@@ -76,7 +76,7 @@ module Caminio
       elsif args[0]        
         I18n.locale = args[0]   
       else
-        I18n.locale = params[:locale] || get_browser_locale || I18n.default_locale
+        I18n.locale = params[:locale] || I18n.default_locale # get_browser_locale || I18n.default_locale
       end
     end
 
