ca2bf47c25be36361bb6b76e906998bace23bf6f
thorsten zerha <thorsten.zerha@tastenwerk.com>
2015-04-09 16:46:44 +0200

    update

--- lib/caminio/timestamps.rb
+++ lib/caminio/timestamps.rb
@@ -7,15 +7,15 @@ module Caminio
 
     included do
 
-      field :created_at, type: DateTime, default: lambda{ DateTime.now }
-      field :updated_at, type: DateTime, default: lambda{ DateTime.now }
+      field :created_at, type: DateTime, default: lambda{ puts "BEFORE"; Time.now ; puts "AFTER "}
+      field :updated_at, type: DateTime, default: lambda{ Time.now }
 
       before_update :set_updated_at
 
-      protected
+      private
 
         def set_updated_at
-          self.updated_at = DateTime.now
+          self.updated_at = Time.now
         end
 
     end
